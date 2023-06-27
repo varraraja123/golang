48b44af9468e28d451ca75e7dde055032db196a9
srinivas-0 <sreenikaluvai@gmail.com>
2023-06-27 15:30:34 +0530

    sample go file with sample variable

---
+++ sample1.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to my booking application")
+	fmt.Println("You can book your tickets here.")
+
+	var name = "booking"
+	fmt.Println(name)
+}
