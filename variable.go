d68d8d198115346abb8646c0259a5d27542a5b94
srinivas-0 <sreenikaluvai@gmail.com>
2023-06-27 15:31:43 +0530

    sample go file with variables defined

---
+++ variable.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Go Conference"
+
+	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("Get your tickets here to attend")
+}
